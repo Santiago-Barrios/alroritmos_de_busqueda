f85866cf786a87dfe0bbbf796c66dc323cbf1df3
Santiago Barrios <santiagobarriosgrijalba.08@gmail.com>
2021-05-10 19:43:06 -0500

    busqueda_binaria y conteo de iteraciones

--- busqueda_lineal.py
+++ busqueda_lineal.py
@@ -1,13 +1,14 @@
 import random
 
-def busqueda_lineal( lista, objetivo):
+def busqueda_lineal( lista, objetivo, iter_lin=0):
     matche = False
 
     for elemento in lista: # O(n)
+        iter_lin += 1
         if elemento == objetivo:
             matche = True
             break
-    return matche
+    return (matche, iter_lin)
 
 def run():
     tamano_de_la_lista = int(input('De que tamaño sera la lista? '))
@@ -15,9 +16,10 @@ def run():
 
     lista=[random.randint(0, 100) for i in range (tamano_de_la_lista)]
 
-    encontrado = busqueda_lineal(lista, objetivo)
+    (encontrado, iter_lin) = busqueda_lineal(lista, objetivo)
     print(lista)
     print(f'El elemento {objetivo} {"esta" if encontrado else "no esta"} en la lista ')
+    print(f'El número de iteraciones fue {iter_lin} ')
 
 if __name__ == '__main__':
     run()
